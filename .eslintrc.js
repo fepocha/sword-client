39f8b52f52502e6922f160f61cb44bde30a97b8a
Juyeon <jy7123943@gmail.com>
2022-02-26 23:40:22 +0900

    Change eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,18 +23,24 @@ module.exports = {
     '@typescript-eslint',
     "prettier",
     "simple-import-sort",
+    "jsx-a11y",
   ],
   rules: {
     "simple-import-sort/exports": "error",
 
     "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react/prop-types": "off",
     "react/jsx-props-no-spreading": "off",
     "react/no-array-index-key": "warn",
     "react/button-has-type": "warn",
     "react/require-default-props": "off",
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
+    "react/jsx-indent": ["error", 2, { "checkAttributes": true, "indentLogicalExpressions": true }],
+    "react/jsx-indent-props": ["error", 2],
+    "react/jsx-max-props-per-line": ["error", { "maximum": { "single": 2, "multi": 1 } }],
+    "react/jsx-first-prop-new-line": ["error", "multiline"],
 
-    "no-trailing-spaces": ["error", { "skipBlankLines": false, "ignoreComments": false }]
+    "no-trailing-spaces": ["error", { "skipBlankLines": false, "ignoreComments": false }],
   },
 };
