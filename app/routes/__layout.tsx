2da5f42f048916097509a2e4a89b6764f2c83840
Juyeon <jy7123943@gmail.com>
2022-02-26 23:58:33 +0900

    Add main tag

--- app/routes/__layout.tsx
+++ app/routes/__layout.tsx
@@ -37,7 +37,9 @@ export default function Layout() {
           </ul>
         </nav>
       </header>
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
       <footer>Footer</footer>
     </>
   );
