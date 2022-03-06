33e2e50a18a6a17bb5f0f2258cfdc2d776727e40
Juyeon <jy7123943@gmail.com>
2022-03-06 20:15:35 +0900

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,49 @@
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) return `rgb(var(${variable}))`;
+
+    return `rgb(var(${variable}) / ${opacityValue})`;
+  };
+}
+
+
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  content: ['./app/**/*.{ts,tsx,jsx,js}'],
+  darkMode: 'class',
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      white: {
+        light: withOpacityValue('--color-white-light'),
+        mid: withOpacityValue('--color-white-mid'),
+        dark: withOpacityValue('--color-white-dark'),
+      },
+      gray: {
+        light: withOpacityValue('--color-gray-light'),
+        mid: withOpacityValue('--color-gray-mid'),
+        dark: withOpacityValue('--color-gray-dark'),
+      },
+      black: {
+        light: withOpacityValue('--color-black-light'),
+        mid: withOpacityValue('--color-black-mid'),
+        dark: withOpacityValue('--color-black-dark'),
+      },
+      orange: {
+        light: withOpacityValue('--color-orange-light'),
+        mid: withOpacityValue('--color-orange-mid'),
+        dark: withOpacityValue('--color-orange-dark'),
+      },
+      blue: {
+        mid: withOpacityValue('--color-blue-light'),
+      },
+    },
   },
   plugins: [],
 }
