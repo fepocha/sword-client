f401fc2f9aa22df5891a1c25ccfd5673865d3827
Juyeon <jy7123943@gmail.com>
2022-03-06 20:28:12 +0900

    Move color files in root directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
-function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) return `rgb(var(${variable}))`;
-
-    return `rgb(var(${variable}) / ${opacityValue})`;
-  };
-}
-
+const colors = require('./colors.js');
 
 module.exports = {
   content: ['./app/**/*.{ts,tsx,jsx,js}'],
@@ -18,32 +11,7 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    colors: {
-      transparent: 'transparent',
-      white: {
-        light: withOpacityValue('--color-white-light'),
-        mid: withOpacityValue('--color-white-mid'),
-        dark: withOpacityValue('--color-white-dark'),
-      },
-      gray: {
-        light: withOpacityValue('--color-gray-light'),
-        mid: withOpacityValue('--color-gray-mid'),
-        dark: withOpacityValue('--color-gray-dark'),
-      },
-      black: {
-        light: withOpacityValue('--color-black-light'),
-        mid: withOpacityValue('--color-black-mid'),
-        dark: withOpacityValue('--color-black-dark'),
-      },
-      orange: {
-        light: withOpacityValue('--color-orange-light'),
-        mid: withOpacityValue('--color-orange-mid'),
-        dark: withOpacityValue('--color-orange-dark'),
-      },
-      blue: {
-        mid: withOpacityValue('--color-blue-light'),
-      },
-    },
+    colors,
   },
   plugins: [],
 }
