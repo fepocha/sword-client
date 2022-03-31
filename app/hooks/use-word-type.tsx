21f242f9ea85cf1a113ac8a6d4c786d2af171874
Juyeon <jy7123943@gmail.com>
2022-03-31 21:36:25 +0900

    Refactor type event reducer with custom hook

---
+++ app/hooks/use-word-type.tsx
@@ -0,0 +1,48 @@
+import { Reducer, useReducer } from 'react';
+
+interface Action {
+  type: 'typeCharacter' | 'deleteCharacter' | 'showHelperText' | 'clearHelperText';
+  payload?: string;
+}
+interface State {
+  word: string;
+  helperText?: string;
+}
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case 'typeCharacter':
+      return { ...state, word: state.word + action.payload };
+    case 'deleteCharacter':
+      return { ...state, word: state.word.slice(0, -1) };
+    case 'showHelperText':
+      return { ...state, helperText: action.payload };
+    case 'clearHelperText':
+      return { ...state, helperText: '' };
+    default:
+      throw new Error();
+  }
+}
+
+export function useWordType() {
+  const [{ word, helperText }, dispatch] = useReducer<Reducer<State, Action>>(reducer, { word: '' });
+
+  const typeCharacter = (character: string) => {
+    if (word.length < 5) dispatch({ type: 'typeCharacter', payload: character });
+  };
+  const deleteCharacter = () => {
+    if (word.length > 0) dispatch({ type: 'deleteCharacter' });
+  };
+  const clearHelperText = () => dispatch({ type: 'clearHelperText' });
+  const showHelperText = (text: string) => dispatch({ type: 'showHelperText', payload: text });
+
+
+  return {
+    word,
+    helperText,
+    typeCharacter,
+    deleteCharacter,
+    clearHelperText,
+    showHelperText,
+  };
+}
--- app/routes/__layout/word/new.tsx
+++ app/routes/__layout/word/new.tsx
@@ -1,51 +1,34 @@
-import { Reducer, useReducer } from 'react';
 import type { Key } from '~/components/word/Keyboard';
 import { Keyboard } from '~/components/word/Keyboard';
 import { WordBlock } from '~/components/word/WordBlock';
+import { useWordType } from '~/hooks/use-word-type';
 
 const HELPER_TEXT = {
   INVALID_LENGTH: 'A word should be 5 characters long',
 };
 
-interface Action {
-  type: 'type' | 'backspace' | 'showHelperText' | 'clearHelperText';
-  payload?: string;
-}
-interface State {
-  word: string;
-  helperText?: string;
-}
-
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case 'type':
-      return { ...state, word: state.word + action.payload };
-    case 'backspace':
-      return { ...state, word: state.word.slice(0, -1) };
-    case 'showHelperText':
-      return { ...state, helperText: action.payload };
-    case 'clearHelperText':
-      return { ...state, helperText: '' };
-    default:
-      throw new Error();
-  }
-}
 export default function NewWordFormPage() {
-  const [{ word, helperText }, dispatch] = useReducer<Reducer<State, Action>>(reducer, { word: '' });
+  const {
+    word,
+    helperText,
+    typeCharacter,
+    deleteCharacter,
+    showHelperText,
+    clearHelperText,
+  } = useWordType();
 
   const handleKeyClick = ({ type, value }: Key) => {
-    dispatch({ type: 'clearHelperText' });
+    clearHelperText();
     if (type === 'character') {
-      if (word.length < 5) dispatch({ type: 'type', payload: value });
+      typeCharacter(value);
       return;
     }
     if (type === 'backspace') {
-      dispatch({ type: 'backspace' });
+      deleteCharacter();
       return;
     }
     if (word.length < 5) {
-      dispatch({ type: 'showHelperText', payload: HELPER_TEXT.INVALID_LENGTH });
+      showHelperText(HELPER_TEXT.INVALID_LENGTH);
     }
     // submit event
   };
