4b6c984fd035556d5fba2ab6d905ff597727b005
elliot <elliot@wired.company>
2022-04-03 15:17:44 +0900

    feat:title changed

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
       <div className="flex flex-col justify-center items-center min-h-screen">
         <div className="py-10">
           <TypewriterText type="h1" className="text-2xl sm:text-4xl lg:text-5xl">
-            Welcome to <span className="text-orange-light">Words-say</span>
+            Welcome to <span className="text-orange-light">Word's say</span>
           </TypewriterText>
         </div>
         <div className="py-3">
