3090b4bbc9f5878e72d8af3fcbe0c46e21e7c872
Juyeon <jy7123943@gmail.com>
2022-04-03 17:00:06 +0900

    Change navigation style

--- app/components/layout/Navigation.tsx
+++ app/components/layout/Navigation.tsx
@@ -1,38 +1,50 @@
 import classNames from 'classnames';
+import { useState } from 'react';
 import { NavLink  } from 'remix';
 
+const NAVIGATION = [
+  {
+    path: '/word/new',
+    displayName: 'Add a new word'
+  },
+  {
+    path: '/word/random',
+    displayName: 'Play Games'
+  },
+  {
+    path: '/tutorial',
+    displayName: 'Tutorial'
+  },
+];
+
 export function Navigation() {
+  const [isExpanded, setExpanded] = useState(true);
+
+  const handleMenuToggle = () => {
+    setExpanded(prevExpanded => !prevExpanded);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink
-            to="/word/new"
-            prefetch="intent"
-            className={({ isActive }) => classNames({ 'active-menu': isActive })}
+    <>
+      <button type="button" onClick={handleMenuToggle}>Menu</button>
+      {isExpanded && (
+        <nav className="fixed top-header left-0 w-full z-10 bg-white-mid text-center h-[calc(100vh-theme(space.header))] py-10">
+          <ul className="grid gap-8">
+            {NAVIGATION.map(({ path, displayName }) => (
+              <li key={path} className="text-2xl">
+                <NavLink
+                  to={path}
+                  prefetch="intent"
+                  className={({ isActive }) => classNames('relative', { 'after:absolute after:bottom-0 after:left-0 after:block after:w-full after:h-2 after:bg-orange-dark after:-z-10': isActive })}
+                  onClick={handleMenuToggle}
               >
-            Add a new word
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/word/random"
-            prefetch="intent"
-            className={({ isActive }) => classNames({ 'active-menu': isActive })}
-              >
-            Play Games
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/tutorial"
-            prefetch="intent"
-            className={({ isActive }) => classNames({ 'active-menu': isActive })}
-              >
-            Tutorial
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+                  {displayName}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </>
   );
 }
--- app/routes/__layout.tsx
+++ app/routes/__layout.tsx
@@ -5,12 +5,12 @@ import { ThemeToggle } from '~/components/layout/ThemeToggle';
 export default function Layout() {
   return (
     <div className="root-container">
-      <header className="flex border-b-[1px] justify-between items-center px-[20px]">
+      <header className="fixed top-0 left-0 w-full h-header flex border-b-[1px] border-gray-light justify-between items-center px-[20px] z-50">
         <Navigation />
         <h1>Wordssay</h1>
         <ThemeToggle />
       </header>
-      <main>
+      <main className="pt-header">
         <Outlet />
       </main>
     </div>
