73f95942c5c9975e10a229f3372225f9d94305f7
Juyeon <jy7123943@gmail.com>
2022-04-10 15:02:45 +0900

    Add Dialog

---
+++ app/components/dialog/ErrorDialog.tsx
@@ -0,0 +1,11 @@
+import { Dialog, DialogContent } from './index';
+import type { DialogContentProps } from './index';
+
+
+export function ErrorDialog({ title = 'Error', description = 'Something went wrong.\nPlease try again.' }: DialogContentProps) {
+  return (
+    <Dialog>
+      <DialogContent title={title} description={description} />
+    </Dialog>
+  );
+}
---
+++ app/components/dialog/index.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren } from 'react';
+
+export interface DialogContentProps {
+  title?: string;
+  description?: string;
+}
+
+export function DialogContent({ title, description }: DialogContentProps) {
+  return (
+    <div>
+      {title && <h2 className="text-xl mb-4 text-gray-dark">{title}</h2>}
+      {description && <p className="whitespace-pre-line leading-7">{description}</p>}
+    </div>
+  );
+}
+
+export function Dialog({ children }: PropsWithChildren<unknown>) {
+  return (
+    <div className="fixed top-0 left-0 w-screen h-screen bg-dim z-50">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[400px] px-4">
+        <div className="w-full h-full bg-white-mid rounded-lg text-center px-6 py-8">
+          {children}
+          <footer className="mt-8">
+            <button type="button" className="bg-orange-dark px-6 py-2 rounded-md active:bg-opacity-80">Close</button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
