89122ef812cbd6bf9e976085f1479044bc7f9c61
Juyeon <jy7123943@gmail.com>
2022-04-10 15:11:27 +0900

    Fix dialog style

--- app/components/dialog/index.tsx
+++ app/components/dialog/index.tsx
@@ -8,7 +8,7 @@ export interface DialogContentProps {
 export function DialogContent({ title, description }: DialogContentProps) {
   return (
     <div>
-      {title && <h2 className="text-xl mb-4 text-gray-dark">{title}</h2>}
+      {title && <h2 className="text-xl mb-4">{title}</h2>}
       {description && <p className="whitespace-pre-line leading-7">{description}</p>}
     </div>
   );
