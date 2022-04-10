4a70aadb636e0a0988515484552910cbde92d11c
Juyeon <jy7123943@gmail.com>
2022-04-10 23:54:45 +0900

    Add dialog props

--- app/components/dialog/ErrorDialog.tsx
+++ app/components/dialog/ErrorDialog.tsx
@@ -1,9 +1,13 @@
 import { Dialog, DialogContent } from './index';
-import type { DialogContentProps } from './index';
+import type { DialogContentProps, DialogProps } from './index';
 
-export function ErrorDialog({ title = 'Error', description = 'Something went wrong.\nPlease try again.' }: DialogContentProps) {
+export function ErrorDialog({
+  title = 'Error',
+  description = 'Something went wrong.\nPlease try again.',
+  onClick,
+}: DialogContentProps & DialogProps) {
   return (
-    <Dialog>
+    <Dialog onClick={onClick}>
       <DialogContent title={title} description={description} />
     </Dialog>
   );
