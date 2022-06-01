bc561df4b22fd93b3e612ae8e9396af63e23eadd
jy7123943 <jy7123943@gmail.com>
2022-06-01 11:40:07 +0900

    Add fetchWordResults

--- app/api/fetch-random-word.ts
+++ app/api/fetch-random-word.ts
@@ -1,18 +1,11 @@
 import axios from 'axios';
-import { getApiBaseUrl } from './index';
+import { getApiBaseUrl, Word } from './index';
 
 export interface IFetchRandomQueryRequest {
   excludedWords: string[]; // 이미 푼 단어
 }
 
-export interface IFetchRandomWordResponse {
-  word: string; // 문제 단어
-  createdBy: string; // 출제자 이름
-  description: string; // 출제자의 한마디
-  createdAt: Date; // 생성일자
-  id: string; // 아이디
-  answerId: string; // answer id
-}
+export type IFetchRandomWordResponse = Word;
 
 export const FETCH_RANDOM_WORDS_API_PATH = `${getApiBaseUrl()}/words/random`;
 
